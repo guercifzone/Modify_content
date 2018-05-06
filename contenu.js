03c016b08a1cd5d21736eca94ff1fbfb4e041c64
Mona Lisa <sadik.fattah@outlook.com>
2018-05-06 15:21:21 +0100

    Modify content

---
+++ contenu.js
@@ -0,0 +1,11 @@
+
+//var td = document.getElementsByClassName('adBlockDetect');
+chrome.tabs.executescript({
+
+ //code: 'document.getElementById("pagelet_ego_pane").style.display = "none";'
+ //code: 'document.getElementsByClassNameClassName("_1qkq")[0].style.display = "none";'
+/*var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://www.google.com/' + images[i].width + '/' + images[i].height;
+}*/
+});
